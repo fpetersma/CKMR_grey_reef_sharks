af8fd702dd859f568e6f37f47f41215e37d49a4a
Felix Petersma <felix.petersma@gmail.com>
2022-07-13 14:05:31 +0100

    added new rdata file for sim

--- source/simulating/GR_sims_mix.R
+++ source/simulating/GR_sims_mix.R
@@ -112,6 +112,7 @@ nrow(indiv[is.na(indiv$DeathY), ])
 # View(indiv[is.na(indiv$DeathY), ])
 
 ## Save data for fitting
+# save.image(file = "data/data_for_testing_estimator.RData")
 
 ## Looking up relationship between captured pairs
 pairs <- findRelativesPar(indiv = indiv, 
