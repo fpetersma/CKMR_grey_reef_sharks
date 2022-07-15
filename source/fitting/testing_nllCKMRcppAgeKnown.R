21672f5307d83b7f296631f1a05c68e8a0780f7b
Felix T Petersma <felix.petersma@gmail.com>
2022-07-15 11:50:17 +0100

    added fitting for 100 simulated data sets

--- source/fitting/testing_nllCKMRcppAgeKnown.R
+++ source/fitting/testing_nllCKMRcppAgeKnown.R
@@ -1,6 +1,12 @@
-#' ========================================================================== ##
-#' Test nllCKMRcppAgeKnown()
-#' ========================================================================== ##
+################################################################################
+##
+##                      Test nllCKMRcppAgeKnown()
+##
+################################################################################
+
+##================================================================
+##                          Single model                         =
+##================================================================
 
 library(Rcpp)
 
@@ -60,3 +66,81 @@ sum(indiv$Sex == "M" & is.na(indiv$DeathY) & indiv$AgeLast >= 10)
 #              dat = dat, 
 #              control = list(trace = 0))
 
+
+
+##================================================================
+##                      One hundred models                       =
+##================================================================
+
+library(Rcpp)
+library(parallel)
+library(pbapply)
+
+load("data/100_sims_dfs_suff.RData")
+
+sourceCpp("source/fitting/nllCKMRcppAgeKnown.cpp")
+
+result_list <- pblapply(dfs_suff, function(df) {
+  par <- list(
+    # phi = boot::logit(0.87), # same as plogis(0.9) -- boot::inv.logit() is qlogis()
+    N_t0_m = log(500), 
+    # r = log(1.03),
+    N_t0_f = log(500))
+  # sigma_vbgf = log(2))
+  
+  df_select <- df[, ]
+  
+  dat <- list(alpha_m = 10, 
+              alpha_f = 12,
+              
+              r = log(1.01),
+              # sigma_vbgf = log(0.000001),
+              phi = boot::logit(0.87),
+              
+              max_age = 19,
+              t0 = 140,
+              # vbgf_l_inf = 175,
+              # vbgf_k = 0.1,
+              # vbgf_t0 = -3.5,
+              s1 = df_select$indiv_1_sex,
+              s2 = df_select$indiv_2_sex,
+              c1 = df_select$indiv_1_capture_year,
+              c2 = df_select$indiv_2_capture_year,
+              a1 = df_select$indiv_1_age,
+              a2 = df_select$indiv_2_age,
+              pair_found = df_select$pop_found,
+              cov_combo_freq = df_select$covariate_combo_freq,
+              n = nrow(df_select))
+  
+  res <- nlminb(start = par, 
+                objective = nllPOPCKMRcppAgeKnown, 
+                dat = dat, 
+                control = list(trace = 1))
+  
+  return(res)
+  
+})
+
+N_male <- sapply(result_list, function(res){
+  res$par["N_t0_m"]
+})
+
+N_female <- sapply(result_list, function(res){
+  res$par["N_t0_f"]
+})
+
+s
+system.time({
+  result_Rcpp <- nllPOPCKMRcppAgeKnown(dat = dat, par = par)
+})
+
+
+
+sum(indiv$Sex == "F" & is.na(indiv$DeathY) & indiv$AgeLast >= 12)
+sum(indiv$Sex == "M" & is.na(indiv$DeathY) & indiv$AgeLast >= 10)
+
+# res <- optim(par = par, 
+#              fn = nllPOPCKMRcpp, 
+#              method = "BFGS",
+#              dat = dat, 
+#              control = list(trace = 0))
--- source/simulating/finding_pairs_bluewhale.R
+++ source/simulating/finding_pairs_bluewhale.R
@@ -8,14 +8,147 @@
 ## Load packages and source custom functions ===================================
 library(fishSim)
 library(ids)
+library(pbapply)
+library(parallel)
+
+source("source/fitting/CKMR_functions.R")
 
 ## Load data
-load(file = "data/data_for_testing_estimator.RData")
+load(file = "data/100_sims_mix.RData")
 
+
+## Find the pairs in parallel. With 40 cores it took roughly 35 minutes
+n_cores <- 40
+cl <- makeCluster(n_cores)
+clusterExport(cl, c("findRelativesPar", "parents"))
+pairs_list <- pblapply(simulated_data_sets, function(indiv) {
+  return(findRelativesPar(indiv = indiv, 
+                          sampled = TRUE, 
+                          nCores = 1))
+}, cl = cl)
+stopCluster(cl)
+
+## Save data
+# save.image("data/100_sims_mix_pairs.RData")
+
+POPs_list <- pblapply(pairs_list, function(pairs) {
+  return(pairs[pairs$OneTwo == 1, ]) ## Parent-Offspring pairs)
+})
+
+## Save data
+# save(list = c("simulated_data_sets", "POPs_list"), 
+#      file =  "data/100_sims_mix_POPs.RData")
+
+
+## Prep data ready for analysis
+combined_data <- lapply(1:100, function(i) {
+  out <- list(POPs = POPs_list[[i]], 
+              indiv = simulated_data_sets[[i]])
+  return(out)
+})
+
+n_cores <- 20
+cl <- makeCluster(n_cores)
+clusterExport(cl, c("vbgf"))
+dfs <- pblapply(combined_data, function(x) {
+  POPs <- x$POPs
+  indiv <- x$indiv
+  
+  ## Extract sampled individuals from the population
+  sampled_indiv <- indiv[!is.na(indiv$SampY),] # a total of 1258 sampled 
+  # individuals = 1581306 comparisons
+  
+  ## Keep relevant information and rename columns to match theory
+  sampled_indiv <- subset(sampled_indiv, select = c(Me, Sex, AgeLast, SampY))
+  colnames(sampled_indiv) <- c("id", "sex", "age", "capture_year")
+  
+  ## In practice age is latent, but length is not. Convert age to length through 
+  ## the VBGF, and add a normal error. Roughly based on other studies.
+  ## Derive the length corresponding to the age, and add error from N(0, 2)
+  sampled_indiv$length <- vbgf(sampled_indiv$age)
+  sampled_indiv$length <- sampled_indiv$length + rnorm(nrow(sampled_indiv), 0, 2)
+  
+  ## Create matrix of all combinations with information
+  df <- subset(expand.grid(sampled_indiv$id, sampled_indiv$id), 
+               Var1 != Var2)
+  colnames(df) <- c("indiv_1_id", "indiv_2_id")
+  
+  ## Add columns with individual capture info, to be filled in in the loop below
+  info_cols <- c("sex", "capture_year", "length", "age")
+  df[, paste0("indiv_1_" , info_cols)] <- NA
+  df[, paste0("indiv_2_" , info_cols)] <- NA
+  
+  ## Loop through the unique ids and add capture information to main df
+  for (id in unique(sampled_indiv$id)) {
+    ## Extract info for individual with id
+    info <- sampled_indiv[sampled_indiv$id == id, info_cols]
+    
+    ## Add info when individual id is first in the comparison
+    df[df$indiv_1_id == id, paste0("indiv_1_", info_cols)] <- info
+    
+    ## Add info when individual id is second in the comparison
+    df[df$indiv_2_id == id, paste0("indiv_2_", info_cols)] <- info
+  }
+  
+  ## Round lengths to nearest integer, as this is how it is recorded.
+  df$indiv_1_length <- round(df$indiv_1_length)
+  df$indiv_2_length <- round(df$indiv_2_length)
+  
+  ## Make sure the right columsn are numeric
+  df$indiv_1_capture_year <- as.numeric(df$indiv_1_capture_year)
+  df$indiv_1_length <- as.numeric(df$indiv_1_length)
+  df$indiv_1_age <- as.numeric(df$indiv_1_age)
+  df$indiv_2_capture_year <- as.numeric(df$indiv_2_capture_year)
+  df$indiv_2_length <- as.numeric(df$indiv_2_length)
+  df$indiv_2_age <- as.numeric(df$indiv_2_age)
+  
+  ## Add unique id for the combination
+  df$comb_id <- paste0(df$indiv_1_id, "_", df$indiv_2_id)
+  POPs$comb_id <- paste0(POPs$Var1, "_", POPs$Var2)
+  
+  ## Add whether a POP was found, or not
+  df$pop_found <- df$comb_id %in% POPs$comb_id
+  
+  ## Find unique cov (using age instead of length)
+  df$covariate_combo_id <- apply(df, 1, function(row) {
+    return(paste0(row[3], row[4], row[6], row[7], row[8], row[10], row[12]))
+  })
+  
+  df$covariate_combo_id <- as.numeric(as.factor(df$covariate_combo_id))
+  
+  ## Good news: only 138,425 unique probabilities to derive, instead of 1,581,306
+  
+  ## Add frequency of every identical covariate combination
+  df <- transform(df, covariate_combo_freq = ave(seq(nrow(df)),
+                                                 covariate_combo_id,
+                                                 FUN = length))
+  
+  # ## Only keep the first of every identical covariate combo
+  # df_sufficient <- df %>% 
+  #   group_by(covariate_combo_id) %>% 
+  #   slice(1) %>% 
+  #   ungroup()
+  
+  return(df)
+  
+}, cl = cl)
+stopCluster(cl)
+
+library(dplyr)
+dfs_suff <- pblapply(dfs, function(x) {
+  df_sufficient <- x %>% 
+    group_by(covariate_combo_id) %>% 
+    slice(1) %>% 
+    ungroup()
+  return(df_sufficient)
+})
+
+# save(list = c("dfs"), file = c("data/100_sims_dfs.RData"))
+# save(list = c("dfs_suff"), file = c("data/100_sims_dfs_suff.RData"))
 ## Looking up relationship between captured pairs
 pairs <- findRelativesPar(indiv = indiv, 
                           sampled = TRUE, 
-                          nCores = 30)
+                          nCores = 6)
 POPs <- pairs[pairs$OneTwo == 1, ] ## Parent-Offspring pairs
 
 ## Save data
