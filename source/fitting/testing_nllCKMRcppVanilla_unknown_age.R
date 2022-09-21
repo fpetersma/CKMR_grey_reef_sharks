9798e796eeab92871170404970de7ac7f82ed986
Felix T Petersma <felix.petersma@gmail.com>
2022-09-21 14:37:52 +0100

    Create testing_nllCKMRcppVanilla_unknown_age.R

---
+++ source/fitting/testing_nllCKMRcppVanilla_unknown_age.R
@@ -0,0 +1,112 @@
+################################################################################
+##
+##                      Test nllCKMRcppVanilla()
+##
+################################################################################
+
+##================================================================
+##                      One thousand models                     =
+##================================================================
+
+library(Rcpp)
+library(parallel)
+library(pbapply)
+source("source/fitting/CKMR_functions.R")
+
+## BE CAREFULL WHICH DATA TO LOAD
+## ==============================
+
+load("data/vanilla_sample_years_139-140_sample_size_375/1000_sims_dfs_suff_age_unique_combos.RData")
+
+## MAKE SURE THE LATEST VERSION OF THE LIKELIHOOD IS COMPILED
+## ==========================================================
+sourceCpp("source/fitting/nllCKMRcppVanilla_unknown_age.cpp")
+
+## Convert ages to length without noise
+dfs_suff <- pblapply(dfs_suff, function(df) {
+  df$indiv_1_length <- round(vbgf(df$indiv_1_capture_age))
+  df$indiv_2_length <- round(vbgf(df$indiv_2_capture_age))
+  return(df)
+})
+
+system.time({
+  result_list <- pblapply(dfs_suff[1], function(df) {
+    par <- list(
+      # phi = boot::logit(0.87), # same as plogis(0.9) -- boot::inv.logit() is qlogis()
+      N_t0_m = log(500), 
+      r = log(1.2),
+      # sigma_l = log(0.01),
+      # phi = boot::logit(1 - 0.153),
+      N_t0_f = log(500))
+
+    df_select <- df[, ]
+    
+    dat <- list(alpha_m = 10,
+                alpha_f = 10,
+                
+                # r = log(1.00),
+                sigma_l = log(0.001),
+                phi = boot::logit(1 - 0.1535),
+                
+                max_age = 19,
+                t0 = 140,
+                vbgf_l_inf = 175,
+                vbgf_k = 0.1,
+                vbgf_a0 = -3.5,
+                s1 = df_select$indiv_1_sex,
+                s2 = df_select$indiv_2_sex,
+                c1 = df_select$indiv_1_capture_year,
+                c2 = df_select$indiv_2_capture_year,
+                a1 = df_select$indiv_1_capture_age,
+                a2 = df_select$indiv_2_capture_age,
+                l1 = round(vbgf(df_select$indiv_1_capture_age)),
+                l2 = round(vbgf(df_select$indiv_2_capture_age)),
+                kinship = df_select$kinship,
+                cov_combo_freq = df_select$covariate_combo_freq,
+                n = nrow(df_select))
+    
+    res <- nlminb(start = par, 
+                  objective = nllPOPCKMRcppAgeUnknown, 
+                  dat = dat, 
+                  control = list(trace = 1))
+    
+    return(res)
+  }) # end of pblapply()
+}) # end of system.time()
+
+## Look at the estimates
+N_est<- t(sapply(result_list, function(res){
+  return(c(N_male = exp(res$par["N_t0_m"]), N_female = exp(res$par["N_t0_f"])))
+}))
+
+r_est <- sapply(result_list, function(res){
+  exp(res$par["r"])
+})
+
+phi_est <- sapply(result_list, function(res){
+  boot::inv.logit(res$par["phi"])
+})
+
+sigma_l_est <- sapply(result_list, function(res){
+  exp(res$par["sigma_l"])
+})
+
+summary(cbind(N_est, r_est, sigma_l_est))
+
+par(mfrow = c(3, 1))
+
+hist(N_est[, 1], main = "", xlab = "Number of mature males"); abline(v = c(mean(N_est[, 1]), median(N_est[, 1])), col = "red", lty = c(1, 2))
+hist(N_est[, 2], main = "",  xlab = "Number of mature females"); abline(v = c(mean(N_est[, 2]), median(N_est[, 2])), col = "red", lty = c(1, 2))
+hist(r_est, main = "", xlab = "Yearly growth rate"); abline(v = c(mean(r_est), median(r_est)), col = "red", lty = c(1, 2))
+# hist(phi_est, main = "", xlab = "Yearly surival rate"); abline(v = c(mean(phi_est), median(phi_est)), col = "red", lty = c(1, 2))
+
+MCE_data <- t(sapply(seq(from = 1, to = 901, by = 100), function(x) {
+  mean_male <- mean(N_est[x:(x+99), 1])
+  mean_female <- mean(N_est[x:(x+99), 2])
+  mean_r <- mean(r_est[x:(x+99)])
+  
+  return(c(mean_male = mean_male, mean_female = mean_female, mean_r = mean_r))
+}))
+
+MCE_100 <- apply(MCE_data, 2, sd)
+MCE_1000 <- MCE_100 / sqrt(10)
