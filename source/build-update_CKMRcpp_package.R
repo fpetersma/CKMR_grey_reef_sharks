5186848a8925d3d9b3fc7930da88caa9311b2011
Felix T Petersma <felix.petersma@gmail.com>
2022-09-26 14:02:32 +0100

    Created CKMRcpp package
    and scripts to support the building and updating of this package

---
+++ source/build-update_CKMRcpp_package.R
@@ -0,0 +1,54 @@
+## =============================================================================
+##                  SCRIPT TO BUILD/UPDATE CKMRcpp PACKAGE
+## 
+## For more info see: 
+## https://cran.r-project.org/web/packages/Rcpp/vignettes/Rcpp-package.pdf
+## =============================================================================
+library(Rcpp)
+
+## :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+##                             FIRST TIME BUILD
+## :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+## 1. Create the skeleton in the correct location
+Rcpp.package.skeleton(name = "CKMRcpp", path = "source")
+
+## 2. Paste the cpp code (e.g., 'nllCKMRcppVanilla.cpp') in the folder 'scr' and 
+## remove the file 'rcpp_hello_world.cpp'. Do not touch the 'RcppExports.cpp' 
+## file. Instead, run the line below which updates this file.
+compileAttributes("source/CKMRcpp")
+
+## 3. Update the DESCRIPTION file if required
+
+## 4. Remove the manual in folder 'man' for the template function
+
+## 5. Build the package using the lines below
+devtools::install("source/CKMRcpp")
+
+## 6. BONUS: to create a tar.gz file which is easy to move around/share, run 
+## the line below
+devtools::build("source/CKMRcpp")
+
+## :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+##                            UPDATE THE PACKAGE
+## :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+## 1. Check if the package exists and can be loaded
+library(CKMRcpp)
+## Unload the package
+detach("package:ascrRcpp", unload=TRUE)
+
+## 2. Make the sure the updated/new cpp files are added to "CKMRcpp/src" 
+
+## 3. Re-compile 
+compileAttributes("source/CKMRcpp")
+
+## 4. Remove the previous installation of the package
+remove.packages("CKMRcpp")
+
+## 5. Install the updated package
+devtools::install("source/CKMRcpp")
+
+## 6. BONUS: to create a tar.gz file which is easy to move around/share, run 
+## the line below
+devtools::build("source/CKMRcpp")
