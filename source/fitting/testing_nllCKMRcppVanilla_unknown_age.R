0d69d4e05401f0593ef5469fc4ef0407f7149bb3
Felix T Petersma <felix.petersma@gmail.com>
2022-09-26 14:03:00 +0100

    Update testing_nllCKMRcppVanilla_unknown_age.R
    Added parallel processing

--- source/fitting/testing_nllCKMRcppVanilla_unknown_age.R
+++ source/fitting/testing_nllCKMRcppVanilla_unknown_age.R
@@ -16,7 +16,9 @@ source("source/fitting/CKMR_functions.R")
 ## BE CAREFULL WHICH DATA TO LOAD
 ## ==============================
 
-load("data/vanilla_sample_years_139-140_sample_size_375/1000_sims_dfs_suff_age_unique_combos.RData")
+file_folder <- "data/vanilla_sample_years_139-140_sample_size_375/"
+
+load(paste0(file_folder, "1000_sims_dfs_suff_age_unique_combos.RData"))
 
 ## MAKE SURE THE LATEST VERSION OF THE LIKELIHOOD IS COMPILED
 ## ==========================================================
@@ -29,53 +31,58 @@ dfs_suff <- pblapply(dfs_suff, function(df) {
   return(df)
 })
 
-system.time({
-  result_list <- pblapply(dfs_suff[1], function(df) {
-    par <- list(
-      # phi = boot::logit(0.87), # same as plogis(0.9) -- boot::inv.logit() is qlogis()
-      N_t0_m = log(500), 
-      r = log(1.2),
-      # sigma_l = log(0.01),
-      # phi = boot::logit(1 - 0.153),
-      N_t0_f = log(500))
+## The unknown age version is a lot slower, so run in parallel
+## Find the pairs in parallel. 
+n_cores <- 20 # 50 fits per core
+cl <- makeCluster(n_cores)
+# clusterExport(cl, c("nllPOPCKMRcppAgeUnknown"))
+result_list <- pblapply(dfs_suff, function(df) {
+  par <- list(
+    # phi = boot::logit(0.87), # same as plogis(0.9) -- boot::inv.logit() is qlogis()
+    N_t0_m = log(500), 
+    r = log(1.2),
+    # sigma_l = log(0.01),
+    # phi = boot::logit(1 - 0.153),
+    N_t0_f = log(500))
+  
+  df_select <- df[, ]
+  
+  dat <- list(alpha_m = 10,
+              alpha_f = 10,
+              
+              # r = log(1.00),
+              sigma_l = log(0.001),
+              phi = boot::logit(1 - 0.1535),
+              
+              max_age = 19,
+              t0 = 140,
+              vbgf_l_inf = 175,
+              vbgf_k = 0.1,
+              vbgf_a0 = -3.5,
+              s1 = df_select$indiv_1_sex,
+              s2 = df_select$indiv_2_sex,
+              c1 = df_select$indiv_1_capture_year,
+              c2 = df_select$indiv_2_capture_year,
+              a1 = df_select$indiv_1_capture_age,
+              a2 = df_select$indiv_2_capture_age,
+              l1 = df_select$indiv_1_length,
+              l2 = df_select$indiv_2_length,
+              kinship = df_select$kinship,
+              cov_combo_freq = df_select$covariate_combo_freq,
+              n = nrow(df_select))
+  
+  res <- nlminb(start = par, 
+                objective = CKMRcpp::nllPOPCKMRcppAgeUnknown, 
+                dat = dat, 
+                control = list(trace = 0))
+  
+  return(res)
+}, cl = cl); stopCluster(cl); # end of pblapply() in parallel
 
-    df_select <- df[, ]
-    
-    dat <- list(alpha_m = 10,
-                alpha_f = 10,
-                
-                # r = log(1.00),
-                sigma_l = log(0.001),
-                phi = boot::logit(1 - 0.1535),
-                
-                max_age = 19,
-                t0 = 140,
-                vbgf_l_inf = 175,
-                vbgf_k = 0.1,
-                vbgf_a0 = -3.5,
-                s1 = df_select$indiv_1_sex,
-                s2 = df_select$indiv_2_sex,
-                c1 = df_select$indiv_1_capture_year,
-                c2 = df_select$indiv_2_capture_year,
-                a1 = df_select$indiv_1_capture_age,
-                a2 = df_select$indiv_2_capture_age,
-                l1 = round(vbgf(df_select$indiv_1_capture_age)),
-                l2 = round(vbgf(df_select$indiv_2_capture_age)),
-                kinship = df_select$kinship,
-                cov_combo_freq = df_select$covariate_combo_freq,
-                n = nrow(df_select))
-    
-    res <- nlminb(start = par, 
-                  objective = nllPOPCKMRcppAgeUnknown, 
-                  dat = dat, 
-                  control = list(trace = 1))
-    
-    return(res)
-  }) # end of pblapply()
-}) # end of system.time()
+# save(list = c("result_list"), file = paste0(file_folder, "result_list_101-200_unknown_age.RData"))
 
 ## Look at the estimates
-N_est<- t(sapply(result_list, function(res){
+N_est <- t(sapply(result_list, function(res){
   return(c(N_male = exp(res$par["N_t0_m"]), N_female = exp(res$par["N_t0_f"])))
 }))
 
@@ -83,15 +90,15 @@ r_est <- sapply(result_list, function(res){
   exp(res$par["r"])
 })
 
-phi_est <- sapply(result_list, function(res){
-  boot::inv.logit(res$par["phi"])
-})
+# phi_est <- sapply(result_list, function(res){
+#   boot::inv.logit(res$par["phi"])
+# })
 
-sigma_l_est <- sapply(result_list, function(res){
-  exp(res$par["sigma_l"])
-})
+# sigma_l_est <- sapply(result_list, function(res){
+#   exp(res$par["sigma_l"])
+# })
 
-summary(cbind(N_est, r_est, sigma_l_est))
+summary(cbind(N_est, r_est))
 
 par(mfrow = c(3, 1))
 
