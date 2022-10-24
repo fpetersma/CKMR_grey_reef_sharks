a67efcab8b9b112b0b40a34df79ec03dd7af9d51
Felix T Petersma <felix.petersma@gmail.com>
2022-10-24 15:08:01 +0100

    Create explore_vbgf_bias_uncertainty.R

---
+++ source/explore_vbgf_bias_uncertainty.R
@@ -0,0 +1,15 @@
+################################################################################
+##                                                                            ##
+##  Explore the different Von Bertalanffy growth functions.                   ##
+##                                                                            ##
+##  Misspecified uncertainty in the length measurements is relatively easy,   ##
+##  but the a 'shift' in the curve is a bit harder. Let's see if I can        ##
+##  achieve what I want to achieve.                                           ##
+##                                                                            ##
+################################################################################
+
+## :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+## Load packages
+## :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+library(tidyverse)
+library(CKMRcpp)
