b79412c78de458bf77ea01c21cb6fb0507bc00cb
Felix T Petersma <felix.petersma@gmail.com>
2022-11-17 18:15:16 +0000

    Update fit_and_visualise_scenarios.R
    visualise scenarios now follows Bradley et al 2017

--- source/fitting/fit_and_visualise_scenarios.R
+++ source/fitting/fit_and_visualise_scenarios.R
@@ -11,20 +11,34 @@
 
 library(pbapply)
 library(parallel)
-load("data/vanilla_gestation_repro=U(1,4)_sample_years_139-140/smaller_files/1000_sims_dfs_suff_length_sd=2_unique_combos.RData")
+data_folder <- "data/palmyra_population/"
 
 ## :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ## Set parameter values
 ## :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-errors <- c(1e-8, 2, 4, 6, 8) # 1e-8 instead of 0 to avoid overflow
+true_error <- 2.89
+
+a_0 <- -8.27 
+k <- 0.0554
+l_inf <- 163
+  
+errors <- c(1e-8, # 1e-8 instead of 0 to avoid overflow
+            true_error*1/3, 
+            true_error*2/3, 
+            true_error, 
+            true_error*4/3, 
+            true_error*5/3, 
+            true_error*6/3) 
 ## vbgfs comes from l.100 onwards in 'explore_vbgf_bias_uncertainty.R'
-step_l_inf <- 8.75  # 8.75 is 5% of 175
-a0_options <- seq(from = 175 - step_l_inf * 3, 
-                  to = 175 + step_l_inf * 3, 
+step_l_inf <- 0.05 * l_inf  # 8.15 is 5% of 163
+a0_options <- seq(from = l_inf - step_l_inf * 3, 
+                  to = l_inf + step_l_inf * 3, 
                   by = step_l_inf)
-vbgfs <- matrix(c(CKMRcpp::a_0_j(175, a0_options, 0.1, -3.5), 
+vbgfs <- matrix(c(CKMRcpp::a_0_j(l_inf, a0_options, 0.0554, -8.27), 
                   a0_options), ncol = 2)
+
+
 
 ## Combine every error with every vbgf parameter combo
 pars <- do.call(rbind, lapply(seq_along(errors), function(i) {
@@ -35,19 +49,19 @@ pars <- do.call(rbind, lapply(seq_along(errors), function(i) {
 }))
 
 ## Number of fitted models for every scenario
-n <- 1000
+n <- 10
 
 ## :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ## Fit all models
 ## :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-# scenario_fits_1_7 <- lapply(1:nrow(pars), function(i) {
-scenario_fits_1_7 <- lapply(1:7, function(i) {
+# scenario_fits <- lapply(1:nrow(pars), function(i) {
+scenario_fits_25 <- lapply(c(25), function(i) {
   a0 <- pars[i, "a_0"]
   l_inf <- pars[i, "l_inf"]
   sigma_l <- pars[i, "sigma_l"]
   
-  n_cores <- 20 # 50 fits per core
+  n_cores <- 10 # 50 fits per core
   cl <- makeCluster(n_cores)
   clusterExport(cl = cl, list("a0", "l_inf", "sigma_l"), envir = environment())
   results <- pblapply(dfs_suff[1:n], function(df) {
@@ -64,20 +78,20 @@ scenario_fits_1_7 <- lapply(1:7, function(i) {
     df_select <- df[, ]
     
     ## Create the data object for nlminb()
-    dat <- list(alpha_m = 10,                         # maturity age for males
-                alpha_f = 12,                         # maturity age for females
+    dat <- list(alpha_m = 17,                         # maturity age for males
+                alpha_f = 19,                         # maturity age for females
                 
-                # r = log(1.0002),
+                # r = log(1.0010),
                 sigma_l = log(sigma_l),
-                phi = boot::logit(1 - 0.097),         # phi is the surival rate
+                phi = boot::logit(1 - 0.1113),         # phi is the surival rate
                 
                 fixed_r = 0,                          # is r fixed or estimated?
                 
-                max_age = 19,
-                max_length = 250,
-                t0 = 140,
+                max_age = 63,
+                max_length = 200,
+                t0 = 2014,
                 vbgf_l_inf = l_inf,
-                vbgf_k = 0.1,
+                vbgf_k = 0.0554,
                 vbgf_a0 = a0,
                 s1 = df_select$indiv_1_sex,
                 s2 = df_select$indiv_2_sex,
@@ -91,16 +105,13 @@ scenario_fits_1_7 <- lapply(1:7, function(i) {
                 cov_combo_freq = df_select$covariate_combo_freq,
                 n = nrow(df_select))
     
-    # system.time(test_new <- nllPOPCKMRcppAgeUnknown(dat, par))
-    # system.time(test_old <- CKMRcpp::nllPOPCKMRcppAgeUnknown(dat, par))
-    
     ## Start the optimisation (make sure the trace and relative tolerance values
     ##                         are correct)
     # system.time({
     res <- nlminb(start = par, 
                   objective = CKMRcpp::nllPOPCKMRcppAgeUnknownGestation, 
                   dat = dat, 
-                  control = list(trace = 0, rel.tol = 1e-10))
+                  control = list(trace = 1, rel.tol = 1e-8))
     # })
     
     return(res)
@@ -109,7 +120,7 @@ scenario_fits_1_7 <- lapply(1:7, function(i) {
 })
 
 ## Save the environment
-save.image("data/simulation_scenarios_results.RData")
+save.image("data/simulation_scenarios_results_r=1.0002.RData")
 
 ## :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ## Visualise the results.
@@ -137,3 +148,14 @@ scenarios$mean_convergence <- conv
 ## UPDATE: when sigma_l = 0, it fails. This makes sense, as it results in a
 ## probability density of positive infinity (due to division by 0). 
 ## Change this to 1e-8.
+result_list <- scenario_fits_25[[1]]
+## Look at the estimates
+N_est <- t(sapply(result_list, function(res){
+  return(c(N_male = exp(res$par["N_t0_m"]), N_female = exp(res$par["N_t0_f"])))
+}))
+
+r_est <- sapply(result_list, function(res){
+  exp(res$par["r"])
+})
+
+summary(cbind(N_est, r_est))
