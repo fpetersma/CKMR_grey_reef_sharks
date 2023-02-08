a8b6c421a454aa2d106275dcd99208442ce40b50
Felix T Petersma <felix.petersma@gmail.com>
2023-02-08 18:17:30 +0000

    created new folder 'general' and ...
    ... moved all the general function there

--- source/general/explore_vbgf_bias_uncertainty.R
+++ source/general/explore_vbgf_bias_uncertainty.R
@@ -18,22 +18,22 @@ library(CKMRcpp)
 ## Create a default vbgf function with plotting capabilities
 ## :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ages <- 0:19
-lengths <- vbgf(ages) # default for vbgf() are t_0=-3.5, k = 0.1, l_inf=175
+lengths <- vbgf(ages) # default for vbgf() are a_0=-3.5, k = 0.1, l_inf=175
 plot(ages, lengths, type = "l", ylim = c(0, 200))
 
 ## Try to do shift it
-t_0_options <- 0:-5
+a_0_options <- 0:-5
 k_options <- seq(from=0.02, to=0.18, by=0.02)
 l_inf_options <- seq(from=160, to=200, by=5)
 
-## t_0
-t_0_matrix <- matrix(NA, nrow = 20, ncol = length(t_0_options))
-for (i in seq_along(t_0_options)) {
-  t_0 <- t_0_options[i]
-  t_0_matrix[, i] <- vbgf(a = ages, t_0 = t_0)
+## a_0
+a_0_matrix <- matrix(NA, nrow = 20, ncol = length(a_0_options))
+for (i in seq_along(a_0_options)) {
+  a_0 <- a_0_options[i]
+  a_0_matrix[, i] <- vbgf(a = ages, a_0 = a_0)
 }
 
-matplot(ages, t_0_matrix, type = "l")
+matplot(ages, a_0_matrix, type = "l")
 
 ## k
 k_matrix <- matrix(NA, nrow = 20, ncol = length(k_options))
@@ -53,51 +53,51 @@ for (i in seq_along(l_inf_options)) {
 
 matplot(ages, l_inf_matrix, type = "l")
 
-## It seems that mainly t_0 and to a lesser extent l_inf are important
+## It seems that mainly a_0 and to a lesser extent l_inf are important
 ## equation 0.5 step to 5 works quite well, but 0.5 to 6.5 is better
-t_0_l_inf_options <- matrix(c(c(-2, -2.5, -3, -3.5, -4, -4.5, -5), 
+a_0_l_inf_options <- matrix(c(c(-2, -2.5, -3, -3.5, -4, -4.5, -5), 
                               seq(from=155.5, to=194.5, by=6.5)), ncol = 2)
-t_0_l_inf_matrix <- matrix(NA, nrow = 20, ncol = nrow(t_0_l_inf_options))
-for (i in 1:nrow(t_0_l_inf_options)) {
-  t_0 <- t_0_l_inf_options[i, 1]
-  l_inf <- t_0_l_inf_options[i, 2]
+a_0_l_inf_matrix <- matrix(NA, nrow = 20, ncol = nrow(a_0_l_inf_options))
+for (i in 1:nrow(a_0_l_inf_options)) {
+  a_0 <- a_0_l_inf_options[i, 1]
+  l_inf <- a_0_l_inf_options[i, 2]
   
-  t_0_l_inf_matrix[, i] <- vbgf(a = ages, l_inf = l_inf, t_0 = t_0, k = 0.15)
+  a_0_l_inf_matrix[, i] <- vbgf(a = ages, l_inf = l_inf, a_0 = a_0, k = 0.15)
 }
 
-matplot(ages, t_0_l_inf_matrix, type = "l", 
+matplot(ages, a_0_l_inf_matrix, type = "l", 
         col = c(rep("black", 3), "red", rep("black", 3)),
         lty = 2, lwd = c(1,1,1,2,1,1,1),
         ylim = c(0, 200))
 
 ## Are the changes in the mean equal?
-diff(colMeans(t_0_l_inf_matrix)) # yeah, pretty much!
-sd(diff(colMeans(t_0_l_inf_matrix))) # 0.01015705
-mean(diff(colMeans(t_0_l_inf_matrix))) # 7.8258 = 4.5%
+diff(colMeans(a_0_l_inf_matrix)) # yeah, pretty much!
+sd(diff(colMeans(a_0_l_inf_matrix))) # 0.01015705
+mean(diff(colMeans(a_0_l_inf_matrix))) # 7.8258 = 4.5%
 
 ## Can I achieve increments of 5% = 8.75?
-t_0_l_inf_options <- matrix(c(seq(from=-2, to=-5, by=-0.5), 
+a_0_l_inf_options <- matrix(c(seq(from=-2, to=-5, by=-0.5), 
                               seq(from=151.4, to=199.1, by=7.7)), ncol = 2)
-t_0_l_inf_matrix <- matrix(NA, nrow = 20, ncol = nrow(t_0_l_inf_options))
-for (i in 1:nrow(t_0_l_inf_options)) {
-  t_0 <- t_0_l_inf_options[i, 1]
-  l_inf <- t_0_l_inf_options[i, 2]
+a_0_l_inf_matrix <- matrix(NA, nrow = 20, ncol = nrow(a_0_l_inf_options))
+for (i in 1:nrow(a_0_l_inf_options)) {
+  a_0 <- a_0_l_inf_options[i, 1]
+  l_inf <- a_0_l_inf_options[i, 2]
   
-  t_0_l_inf_matrix[, i] <- vbgf(a = ages, l_inf = l_inf, t_0 = t_0, k = 0.15)
+  a_0_l_inf_matrix[, i] <- vbgf(a = ages, l_inf = l_inf, a_0 = a_0, k = 0.15)
 }
 
-matplot(ages, t_0_l_inf_matrix, type = "l", 
+matplot(ages, a_0_l_inf_matrix, type = "l", 
         col = c(rep("black", 3), "red", rep("black", 3)),
         lty = 2, lwd = c(1,1,1,2,1,1,1),
         ylim = c(0, 200))
 
 ## Are the changes in the mean equal?
-diff(colMeans(t_0_l_inf_matrix)) # yeah, pretty much, althought not as good as before.
-sd(diff(colMeans(t_0_l_inf_matrix))) # 0.06975593
-mean(diff(colMeans(t_0_l_inf_matrix))) # 8.769889
-diff(colMeans(t_0_l_inf_matrix)) / 175 # Pretty damn close to 5%.
+diff(colMeans(a_0_l_inf_matrix)) # yeah, pretty much, althought not as good as before.
+sd(diff(colMeans(a_0_l_inf_matrix))) # 0.06975593
+mean(diff(colMeans(a_0_l_inf_matrix))) # 8.769889
+diff(colMeans(a_0_l_inf_matrix)) / 175 # Pretty damn close to 5%.
 # closer to 6.3% if looked at the mean of the colMeans, which is probably better
-diff(colMeans(t_0_l_inf_matrix)) / mean(colMeans(t_0_l_inf_matrix)) 
+diff(colMeans(a_0_l_inf_matrix)) / mean(colMeans(a_0_l_inf_matrix)) 
 
 ## :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ## Simulations are at k = 0.1, so can I find the right balance there?
@@ -110,24 +110,24 @@ a_0_j <- function(l_inf_i, l_inf_j, k, a_0_i) {
   return(a_0)
 }
 a0_options <- seq(from=175-8.75*3, to=175+8.75*3, by=8.75)
-t_0_l_inf_options <- matrix(c(a_0(175, a0_options, 0.1, -3.5), 
+a_0_l_inf_options <- matrix(c(a_0_j(175, a0_options, 0.1, -3.5), 
                               a0_options), 
                               ncol = 2)
-t_0_l_inf_matrix <- matrix(NA, nrow = 20, ncol = nrow(t_0_l_inf_options))
-for (i in 1:nrow(t_0_l_inf_options)) {
-  t_0 <- t_0_l_inf_options[i, 1]
-  l_inf <- t_0_l_inf_options[i, 2]
+a_0_l_inf_matrix <- matrix(NA, nrow = 20, ncol = nrow(a_0_l_inf_options))
+for (i in 1:nrow(a_0_l_inf_options)) {
+  a_0 <- a_0_l_inf_options[i, 1]
+  l_inf <- a_0_l_inf_options[i, 2]
   
-  t_0_l_inf_matrix[, i] <- vbgf(a = ages, l_inf = l_inf, t_0 = t_0, k = 0.1)
+  a_0_l_inf_matrix[, i] <- vbgf(a = ages, l_inf = l_inf, a_0 = a_0, k = 0.1)
 }
 
-matplot(ages, t_0_l_inf_matrix, type = "l", 
+matplot(ages, a_0_l_inf_matrix, type = "l", 
         col = c(rep("black", 3), "red", rep("black", 3)),
         lty = 2, lwd = c(1,1,1,2,1,1,1),
         ylim = c(0, 200))
 
 ## Are the changes in the mean equal?
-diff(colMeans(t_0_l_inf_matrix)) # yeah, pretty much, although not as good as before.
-sd(diff(colMeans(t_0_l_inf_matrix))) # 0.06975593
-mean(diff(colMeans(t_0_l_inf_matrix))) # 8.769889
-diff(colMeans(t_0_l_inf_matrix)) / mean(colMeans(t_0_l_inf_matrix)) # Pretty damn close to 5%. 
+diff(colMeans(a_0_l_inf_matrix)) 
+sd(diff(colMeans(a_0_l_inf_matrix))) 
+mean(diff(colMeans(a_0_l_inf_matrix))) 
+diff(colMeans(a_0_l_inf_matrix)) / mean(colMeans(a_0_l_inf_matrix)) 
